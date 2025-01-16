fda80fd45abafd95a7b494ef288cd91625e63b5f
Gym Ubwenge <gymubwenge@Ubwenges-iMac.local>
2025-01-16 13:21:16 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html', // Include all HTML files in the public directory
+    './src/**/*.{html,js}' // Include all HTML and JS files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
